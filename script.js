f4ff42b368535547d3a5d68e13f025e02b617de2
FedeNZ <fedenz.dev@gmail.com>
2022-06-15 11:24:02 -0300

    completed

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function change(par) {
+    if (par === 1) {
+        document.getElementById("pi").innerHTML = "Not anymore"
+    } else {
+        document.getElementById("pi").innerHTML = "This is a parragraph"
+    }
+}
+
+
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
+console.log(Math.floor(Math.random()*2 +1));
