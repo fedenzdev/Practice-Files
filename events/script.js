50d4d81d2c96756702c77c90ab59589884429441
FedeNZ <fedenz.dev@gmail.com>
2022-06-18 13:28:22 -0300

    other options

--- events/script.js
+++ events/script.js
@@ -1,4 +1,15 @@
-const btn = document.querySelector('#btn');
+/*const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
   alert("Hello World");
 });
+*/
+
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+  }
+
+  // METHOD 2
+  btn.onclick = alertFunction;
+  
+  // METHOD 3
+//  btn.addEventListener('click', alertFunction);
